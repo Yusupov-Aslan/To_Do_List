de5f7af45915ab6f5e42dc9bf8be4e54a4a746a5
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2021-12-23 11:56:24 +0300

    создана модель Task и создана миграция

--- source/To_Do_list/admin.py
+++ source/To_Do_list/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from To_Do_list.models import Task
+
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ['id', 'status', 'description', 'created_at']
+    list_filter = ['status']
+    search_fields = ['status']
+    fields = ['status', 'description', 'created_at', 'to_do_at']
+    readonly_fields = ['created_at']
+
+
+admin.site.register(Task, TaskAdmin)
