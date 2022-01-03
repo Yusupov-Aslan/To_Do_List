00c6731cf2c05f2c1bf7840c3554bfa71a305898
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-01-03 16:19:26 +0600

    Добавил представление в views.py one_task_view, создал html страницу one_task.html, добавил путь в urls.py, внес изменения в страничку task_view.html

--- source/To_Do_list/urls.py
+++ source/To_Do_list/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from To_Do_list.views import index_view, add_task_view, view_tasks_view
+from To_Do_list.views import index_view, add_task_view, view_tasks_view, one_task_view
 
 urlpatterns = [
     path('', index_view),
     path('new_task/', add_task_view),
-    path('view_tasks/', view_tasks_view)
+    path('view_tasks/', view_tasks_view),
+    path('task/<int:pk>/', one_task_view)
 ]
--- source/To_Do_list/views.py
+++ source/To_Do_list/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from To_Do_list.models import Task, STATUS_CHOICES
 
 
@@ -18,11 +16,17 @@ def add_task_view(request):
         to_do_at = request.POST.get('to_do_at')
         new_task = Task.objects.create(status=status, description=description,
                                        detailed_description=detailed_description, to_do_at=to_do_at)
-
-        return render(request, 'tasks_add.html', {"tasks": new_task})
+        context = {"tasks": new_task}
+        return render(request, 'tasks_add.html', context)
 
 
 def view_tasks_view(request):
     task = Task.objects.order_by("created_at")
-    return render(request, 'tasks_view.html', {'tasks': task})
+    context = {'tasks': task}
+    return render(request, 'tasks_view.html', context)
 
+
+def one_task_view(request, pk):
+    task = Task.objects.get(pk=pk)
+    context = {"task": task}
+    return render(request, 'one_task.html', context)
