21a2a7aa88dbd62f6a44f802d83a1681ad46d96b
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-01-03 17:48:43 +0600

    В views.py импортировал redirect и Http404, в index.html,one_task.html,tasks_view.html,task_create.html добавил ссылку {%url%}, в urls.py внес имена, удалил tasks_add.html.

--- source/To_Do_list/urls.py
+++ source/To_Do_list/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from To_Do_list.views import index_view, add_task_view, view_tasks_view, one_task_view
 
 urlpatterns = [
-    path('', index_view),
-    path('new_task/', add_task_view),
-    path('view_tasks/', view_tasks_view),
-    path('task/<int:pk>/', one_task_view)
+    path('', index_view, name='index'),
+    path('new_task/', add_task_view, name='add_task_view'),
+    path('view_tasks/', view_tasks_view, name='view_tasks_view'),
+    path('task/<int:pk>/', one_task_view, name='one_task_view')
 ]
--- source/To_Do_list/views.py
+++ source/To_Do_list/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from To_Do_list.models import Task, STATUS_CHOICES
-
+from django.http import Http404
 
 def index_view(request):
     return render(request, 'index.html')
 
 
-def add_task_view(request):
+def add_task_view(request, **kwargs):
     if request.method == 'GET':
         return render(request, 'task_create.html', {"status": STATUS_CHOICES})
     else:
@@ -16,17 +16,27 @@ def add_task_view(request):
         to_do_at = request.POST.get('to_do_at')
         new_task = Task.objects.create(status=status, description=description,
                                        detailed_description=detailed_description, to_do_at=to_do_at)
-        context = {"tasks": new_task}
-        return render(request, 'tasks_add.html', context)
+        # context = {"tasks": new_task}
+        # url = reverse("one_task_view", kwargs={"pk": new_task.pk})
+        # return HttpResponseRedirect(url)
+
+        return redirect("one_task_view", pk=new_task.pk)
+
+        # return render(request, 'tasks_add.html', context)
 
 
 def view_tasks_view(request):
     task = Task.objects.order_by("created_at")
     context = {'tasks': task}
     return render(request, 'tasks_view.html', context)
+    # return redirect("view_tasks_view", context)
 
 
 def one_task_view(request, pk):
-    task = Task.objects.get(pk=pk)
-    context = {"task": task}
+    try:
+        task = Task.objects.get(pk=pk)
+        context = {"task": task}
+    except Task.DoesNotExist:
+        raise Http404
+
     return render(request, 'one_task.html', context)
