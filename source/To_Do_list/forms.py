b2996e4dc6a994dc788305b1893baf6e341e96bf
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-01-16 00:29:28 +0600

    добавил в forms.py type ModelMultipleChoiceField

--- source/To_Do_list/forms.py
+++ source/To_Do_list/forms.py
@@ -5,7 +5,7 @@ from To_Do_list.models import Type, Status
 
 
 class TaskForm(forms.Form):
-    type = forms.ModelChoiceField(queryset=Type.objects.all())
+    type = forms.ModelMultipleChoiceField(queryset=Type.objects.all(), widget=forms.CheckboxSelectMultiple())
     status = forms.ModelChoiceField(queryset=Status.objects.all())
     summary = forms.CharField(max_length=20, required=True, label="Краткое описание")
     description = forms.CharField(max_length=2000, required=False, label="Полное описание",
