9272a374f6b02c312587f6659fdcf890981e5dcf
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-02-11 14:36:08 +0600

    В forms.py сделал поле email обязательным, в base.html в навбар добавил ссылку регистрации!

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -2,6 +2,9 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class MyUserCreationForm(UserCreationForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].required = True
+
     class Meta(UserCreationForm.Meta):
         fields = ("username", "password1", "password2", "email", "first_name", "last_name")
-
