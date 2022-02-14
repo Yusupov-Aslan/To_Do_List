c4ad4f579a006dded99029851c5ced7ecef2f5de
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-02-14 15:07:32 +0600

    Добавил в форму валидатор, который проверяет, что хотя бы одно из полей (first_name, last_name) заполнено. Сделал поле email в форме обязательным для заполнения.

--- source/accounts/forms.py
+++ source/accounts/forms.py
@@ -8,3 +8,11 @@ class MyUserCreationForm(UserCreationForm):
 
     class Meta(UserCreationForm.Meta):
         fields = ("username", "password1", "password2", "email", "first_name", "last_name")
+
+    def is_valid(self):
+        super().is_valid()
+        data = self.cleaned_data
+        if not data.get('first_name') and not data.get('last_name'):
+            self.add_error('first_name', 'Одно из полей Имени или Фамилии должно быть заполнено')
+            return False
+        return True
