c908aa2a929aeb2acb2523cab3b55a828aa6e163
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-02-20 14:46:29 +0600

    Добавил вывод пользователей на странице проекта

--- source/To_Do_list/views/projects.py
+++ source/To_Do_list/views/projects.py
@@ -29,6 +29,11 @@ class ProjectDetailView(DetailView):
     template_name = 'projects/project.html'
     model = Project
 
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['participants'] = self.object.participants.all()
+    #     return context
+
 
 class ProjectTaskAdd(LoginRequiredMixin, CreateView):
     model = Task
