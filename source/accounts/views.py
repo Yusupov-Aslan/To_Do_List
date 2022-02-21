adbd6dc508295eb7623ce9698f518032b2475f03
ваше Yusupov-Aslan <yusupovaslan4@gmail.com>
2022-02-21 18:58:33 +0600

    реализовал личный кабинет

--- source/accounts/views.py
+++ source/accounts/views.py
@@ -1,8 +1,9 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout, get_user_model
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 
 from accounts.forms import MyUserCreationForm
 
@@ -45,3 +46,11 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect('To_Do_list:index')
+
+
+class UserProfileView(LoginRequiredMixin, DetailView):
+    model = get_user_model()
+    template_name = "profile.html"
+    context_object_name = "user_object"
+    paginate_related_by = 5
+    paginate_related_orphans = 0
